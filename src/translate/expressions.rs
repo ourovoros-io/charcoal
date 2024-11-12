077eb5517c73274fe8b2190744507f523dd4afcb
GeorgiosDelkos <georgios.delkos@ourovoros.io>
2024-11-12 18:21:21 +0200

    Fix Parenthesis translation

--- src/translate/expressions.rs
+++ src/translate/expressions.rs
@@ -708,10 +708,36 @@ pub fn translate_parenthesis_expression(
     scope: Rc<RefCell<TranslationScope>>,
     expression: &solidity::Expression,
 ) -> Result<sway::Expression, Error> {
-    // (x)
-    Ok(sway::Expression::Tuple(vec![
-        translate_expression(project, translated_definition, scope.clone(), expression)?,
-    ]))
+    let sway_expr = translate_expression(project, translated_definition, scope.clone(), expression)?;
+    
+    match &expression {
+        solidity::Expression::Assign(_, lhs, _) => {
+            // function add(uint x, uint y) internal pure returns (uint z) {
+            //    require((z = x + y) >= x, "ds-math-add-overflow");
+            // }
+            //
+            // fn add(x: u256, y: u256) -> u256 {
+            //    let mut z: u256 = 0;
+            //    require({z = x + y; z } >= x, "ds-math-add-overflow");
+            //    z
+            // }
+            //
+            let sway_lhs = translate_expression(project, translated_definition, scope.clone(), lhs)?;
+            
+            Ok(sway::Expression::from(sway::Block {
+                statements: vec![
+                    sway::Statement::from(sway_expr),
+                ],
+                final_expr:  Some(sway_lhs),
+            }))
+        },
+        _ => {
+            // (x)
+            Ok(sway::Expression::Tuple(vec![
+                sway_expr,
+            ]))
+        }
+    }
 }
 
 #[inline]
--- tests/test_coverage.rs
+++ tests/test_coverage.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use colorize::AnsiColor;
 
 #[test]
