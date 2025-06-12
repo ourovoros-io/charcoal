83d44afa6612b819391096cc66e0cb56f2e16df8
GeorgiosDelkos <georgios.delkos@ourovoros.io>
2025-06-12 18:13:05 +0300

    chore: forge init

--- tests/code_samples_x/src/Counter.sol
+++ tests/code_samples_x/src/Counter.sol
@@ -1,14 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    uint256 public count;
 
     function increment() public {
-        number++;
+        count += 1;
     }
 }
